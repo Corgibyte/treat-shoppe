650f770b26c8a84a0b271d6c28d5756a511cf52f
Hannah Young <hannah@corgibyte.com>
2022-01-14 10:21:06 -0800

    Fix incorrect property name

--- TreatShoppe/Models/OrderTreat.cs
+++ TreatShoppe/Models/OrderTreat.cs
@@ -8,6 +8,6 @@ namespace TreatShoppe.Models
     public int OrderId { get; set; }
     public int TreatId { get; set; }
     public virtual Order Order { get; set; }
-    public virtual Treat Treats { get; set; }
+    public virtual Treat Treat { get; set; }
   }
 }
