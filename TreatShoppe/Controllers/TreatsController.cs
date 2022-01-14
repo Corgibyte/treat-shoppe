ab437329639146fc9a176fe1708574646ee6aaa8
Hannah Young <hannah@corgibyte.com>
2022-01-14 11:18:50 -0800

    Implement add flavor functionality to treats

--- TreatShoppe/Controllers/TreatsController.cs
+++ TreatShoppe/Controllers/TreatsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using TreatShoppe.Models;
@@ -68,6 +69,33 @@ namespace TreatShoppe.Controllers
       _db.Treats.Remove(thisTreat);
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult AddFlavor(int id)
+    {
+      Treat thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFlavor(Treat treat, int flavorId)
+    {
+      if (flavorId != 0)
+      {
+        _db.FlavorTreats.Add(new FlavorTreat() { FlavorId = flavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = treat.TreatId });
+    }
+
+    [HttpPost]
+    public ActionResult DeleteFlavor(int flavorTreatId)
+    {
+      FlavorTreat thisFlavorTreat = _db.FlavorTreats.FirstOrDefault(flavorTreat => flavorTreat.FlavorTreatId == flavorTreatId);
+      _db.FlavorTreats.Remove(thisFlavorTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = thisFlavorTreat.TreatId });
     }
   }
 }
